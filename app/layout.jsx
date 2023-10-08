759a2fe41d5f405e263a459fcc1a11328af99421
vidhusan12 <vidhusan709@gmail.com>
2023-10-08 18:42:35 +1300

    Created a Lgo

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,9 @@
 import '@styles/globals.css';
 
+import Nav from '@components/Nav';
+import Provider from '@components/Provider';
+
+
 export const metadata = {
     title: "Promptopia",
     description: 'Discover & Share AI Prompts'
@@ -13,6 +17,7 @@ const RootLayout = ({children}) => {
 
             </div>
             <main className="app">
+                <Nav />
                 {children}
             </main>
         </body>
